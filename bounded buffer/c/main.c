431113482d441049398cb1e4b100d9783503cc02
Laurenz Kirchmayr <kirchmayr.laurenz@gmail.com>
2025-01-10 23:28:39 +0100

    solves q5

--- bounded buffer/c/main.c
+++ bounded buffer/c/main.c
@@ -13,19 +13,17 @@ struct BoundedBuffer {
     pthread_mutex_t     mtx;
     sem_t               numElements;
     sem_t               capacity;
-    
-    
 };
 
 struct BoundedBuffer* buf_new(int size){
     struct BoundedBuffer* buf = malloc(sizeof(struct BoundedBuffer));
     buf->buf = rb_new(size);
-    
+
     pthread_mutex_init(&buf->mtx, NULL);
-    // TODO: initialize semaphores
-    //sem_init(&buf->capacity,      0, /*starting value?*/);
-	//sem_init(&buf->numElements,   0, /*starting value?*/);
-    
+
+    sem_init(&buf->capacity,      0, size);
+    sem_init(&buf->numElements,   0, 0);
+
     return buf;    
 }
 
@@ -41,20 +39,21 @@ void buf_destroy(struct BoundedBuffer* buf){
 
 
 void buf_push(struct BoundedBuffer* buf, int val){    
-    // TODO: wait for there to be room in the buffer
-    // TODO: make sure there is no concurrent access to the buffer internals
-    
+    sem_wait(&buf->capacity);
+    pthread_mutex_lock(&buf->mtx);
     rb_push(buf->buf, val);
-    
-    
-    // TODO: signal that there are new elements in the buffer    
+    pthread_mutex_unlock(&buf->mtx);
+    sem_post(&buf->numElements);
 }
 
 int buf_pop(struct BoundedBuffer* buf){
-    // TODO: same, but different?
-    
-    int val = rb_pop(buf->buf);    
-    
+
+    sem_wait(&buf->numElements);
+    pthread_mutex_lock(&buf->mtx);
+    int val = rb_pop(buf->buf);
+    pthread_mutex_unlock(&buf->mtx);
+    sem_post(&buf->capacity);
+
     return val;
 }
 
@@ -64,7 +63,7 @@ int buf_pop(struct BoundedBuffer* buf){
 
 void* producer(void* args){
     struct BoundedBuffer* buf = (struct BoundedBuffer*)(args);
-    
+
     for(int i = 0; i < 10; i++){
         nanosleep(&(struct timespec){0, 100*1000*1000}, NULL);
         printf("[producer]: pushing %d\n", i);

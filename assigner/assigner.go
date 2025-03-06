bcaaeedd3d7bde575a5e0ea93ce2b17cc24a0568
JakobSO <jakobsolheim1608@gmail.com>
2025-03-06 15:00:15 +0100

    Start of cost function

--- assigner/assigner.go
+++ assigner/assigner.go
@@ -4,36 +4,49 @@ import (
 	"elevator/elevio"
 	"net"
 	"time"
+	"elevator/statesync"
 )
-
 
 // TODO: Add a voting system for master election (RAFT)
 // Add RAFT in a seperate file? It is not super easy to implement, but it would be nice for our system
-
 
 const broadcastAddr = "255.255.255.255"
 const broadcastPort = "20068"
 
 type Assignment struct {
 	ElevatorID int
-	Floor int
-	Button ButtonType
+	Floor      int
+	Button     ButtonType
 }
-
 
 // Given a call (floor, button) and a set of elevator states, returns the best elevator ID.
 func Cost(call elevio.ButtonEvent, aliveElevators []int) int {
-    // TODO:
-    // 1. For each elevator in aliveElevators, compute a cost based on:
-    //    - Distance to the call floor
-    //    - Current direction / load
-    //    - Other calls in queue
-	//	  An example cost function exists on blackboard, but would have to be rewritten in Go. 
-    // 2. Pick the elevator with the lowest cost. Break ties by elevator ID if needed.
-    // 3. Return the chosen elevator ID.
-    return 0
-}
+	// TODO:
+	// 1. For each elevator in aliveElevators, compute a cost based on:
+	//    - Distance to the call floor
+	//    - Current direction / load
+	//    - Other calls in queue
+	//	  An example cost function exists on blackboard, but would have to be rewritten in Go.
+	// 2. Pick the elevator with the lowest cost. Break ties by elevator ID if needed.
+	// 3. Return the chosen elevator ID.
+	for _, elevatorID := range aliveElevators {
+		state := GetElevatorState(elevatorID);
+		cost := 0;
+		if state.Floor < call.Floor {
+			cost += call.Floor - state.Floor
+			if state.currDirection == 
 
+		} else {
+			cost += state.Floor - call.Floor
+}
+		
+		// Distance to call floor
+	
+
+
+	}		
+	return 0
+}
 
 func Assign(request elevio.ButtonEvent) {
 	// TODO, Jakob, make cost function
@@ -42,18 +55,18 @@ func Assign(request elevio.ButtonEvent) {
 	aliveIDs := GetAliveElevatorIDs()
 	//Go through the costs of all elevators in loop with. Lowest wins.
 
-	var winnerElevatorID int 
+	var winnerElevatorID int
 
 	addr := broadcastAddr + ":" + broadcastPort
 	conn, _ := net.Dial("udp", addr)
-	
+
 	defer conn.Close()
 	//
 
 	assignment := Assigment{
 		ElevatorID: winnerElevatorID,
-		Floor: request.Floor,
-		Button: request.Button,
+		Floor:      request.Floor,
+		Button:     request.Button,
 	}
 
 	conn.Write(serializeAssignment(assignment))
@@ -67,41 +80,41 @@ func serializeAssignment(assignment Assignment) []byte {
 	return buf
 }
 
-func deserializeAssignment(m byte[]) Assignment {  // @JakobSO Is this right? a list of bytes is defined []byte in Golang afaik
+func deserializeAssignment(m []byte) Assignment { // @JakobSO Is this right? a list of bytes is defined []byte in Golang afaik
 	assignment := Assignment{
 		ElevatorID: m[0],
-		Floor: m[1],
-		Button: m[3],
+		Floor:      m[1],
+		Button:     m[3],
 	}
 	return assignment
 }
 
 func ReceiveAssignments(assignmentChan chan elevio.ButtonEvent, thisElevatorID int) {
-	addr, _ := net.ResolveUDPAddr("udp", broadcastAddr + ":" + broadcastPort)
+	addr, _ := net.ResolveUDPAddr("udp", broadcastAddr+":"+broadcastPort)
 	conn, _ := net.ListenUDP("udp", addr)
-	
+
 	defer conn.Close()
-	
+
 	buf := make([]byte, 128)
-	
+
 	for {
 		n, _, _ := conn.ReadFromUDP(buf)
 		assignment := deserialize(buf[:n])
 		if assignment.ElevatorID == thisElevatorID {
-			// NOTE laurenzk maybe we could just send ButtonEvent here - then we can handle it same 
+			// NOTE laurenzk maybe we could just send ButtonEvent here - then we can handle it same
 			// way as regular button press in elevator controller loop
 			assignmentChan <- assignment
 		}
-	}	
+	}
 }
 
 // This function should be called by non-master elevators whenever a new hall call is registered.
 // The master should then evaluate the call and assign it to an elevator (via Assign).
 func NotifyMasterOfNewHallCall(elevID int, floor int, button elevio.ButtonType) {
-    // TODO: 
-    // 1. Send a message (UDP, TCP, or statesync) to the master elevator indicating a new hall call.
-    // 2. The master, upon receiving this, will run the assignment algorithm (cost function, etc.).
-    // 3. If this elevator is itself the master, it might short-circuit and just call `Assign(...)`.
+	// TODO:
+	// 1. Send a message (UDP, TCP, or statesync) to the master elevator indicating a new hall call.
+	// 2. The master, upon receiving this, will run the assignment algorithm (cost function, etc.).
+	// 3. If this elevator is itself the master, it might short-circuit and just call `Assign(...)`.
 }
 
 // If the disconnected elevator recovers at the same time you're reassigning tasks
@@ -109,24 +122,24 @@ func NotifyMasterOfNewHallCall(elevID int, floor int, button elevio.ButtonType) 
 //
 // This function should be called when an elevator times out or fails a heartbeat.
 func ReassignTasksForDisconnectedElevator(disconnectedID int) {
-    // TODO:
-    // 1. Retrieve all tasks currently assigned to 'disconnectedID'.
-    // 2. Mark them as unassigned or move them into a queue of unassigned tasks.
-    // 3. Re-run the assignment logic (cost function) for each of those tasks.
+	// TODO:
+	// 1. Retrieve all tasks currently assigned to 'disconnectedID'.
+	// 2. Mark them as unassigned or move them into a queue of unassigned tasks.
+	// 3. Re-run the assignment logic (cost function) for each of those tasks.
 }
 
 // The elevator is disconnected from the network but can still move locally.
-// Its rare but possible. If we let it take hall calls we cannot ensure it will not lose them. 
-// We might want to let it handle cab calls only. 
+// Its rare but possible. If we let it take hall calls we cannot ensure it will not lose them.
+// We might want to let it handle cab calls only.
 // (iirc Task description said elevators should be able to run "solo mode" if they get isolated)
 //
 // This function is called periodically if the elevator is offline, to decide whether it should
 // handle new calls locally or remain idle.
 func HandleLocalCallsWhenIsolated(elevID int) {
-    // TODO:
-    // 1. Check if we have connectivity to the master or any peer.
-    // 2. If fully isolated, handle cab calls in a minimal way (stop at floors pressed inside the cabin).
-    // 3. Log or queue hall calls so they can be broadcast if/when we reconnect? 
+	// TODO:
+	// 1. Check if we have connectivity to the master or any peer.
+	// 2. If fully isolated, handle cab calls in a minimal way (stop at floors pressed inside the cabin).
+	// 3. Log or queue hall calls so they can be broadcast if/when we reconnect?
 	// (Can cause duplicates if buttons get pressed on other elevators)
 }
 
@@ -137,7 +150,7 @@ func HandleLocalCallsWhenIsolated(elevID int) {
 //
 // Insert a new call into the unassigned queue, to be handled when a master is present.
 func AddUnassignedTask(call elevio.ButtonEvent) {
-    // TODO:
-    // 1. Push the call onto a queue or list of unassigned tasks.
-    // 2. Optionally broadcast that a new unassigned task is pending (so the master can pick it up).
+	// TODO:
+	// 1. Push the call onto a queue or list of unassigned tasks.
+	// 2. Optionally broadcast that a new unassigned task is pending (so the master can pick it up).
 }

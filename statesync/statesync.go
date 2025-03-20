6663ae02e4fe55e623c45bc3d15ad11f535b4ee7
JakobSO <jakobsolheim1608@gmail.com>
2025-03-20 15:25:47 +0100

    Stuck elevator function

--- statesync/statesync.go
+++ statesync/statesync.go
@@ -306,9 +306,21 @@ func (e *elevatorState) GetRequests() [][3]bool {
 }
 
 // We need to detect when an elevator is stuck, and reassign its Hall calls, otherwise we will stall the system
-func ElevatorStuck() {
+func (e *elevatorState) ElevatorStuck() {
 	// TODO:
 	// 1. Get the state of that elevaotr. If it is idle, or the door is open on a floor, this elevator is not stuck
 	// 2. If the elevator is moving, check when the last time was that the floor changed.
 	// 2. IF the floor has not changed in a while, Assume elevtator is stuck and go offline.
+	currDirection := e.GetDirection()
+	if currDirection == 0 {
+		//Do nothing
+	} else{
+		if time.Since(e.lastActionTime) > 5{
+			e.online = false
+		}
+	}
+}
+//Updates the lastActionTime of an elevator if it changes direction or floor
+func (e *elevatorState) lastAction() {
+	
 }
